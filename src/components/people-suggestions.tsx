9f41c787db3a62b35a9f4e94265781b358e38a84
boludedaniel <boludedaniel@gmail.com>
2025-06-05 00:05:23 +0100

    fixed casing errors

--- src/components/people-suggestions.tsx
+++ src/components/people-suggestions.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/Button";
+import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 interface Suggestion {
